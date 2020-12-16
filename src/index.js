25c1a36754ce55a96487c3db449daec452ad52dd
Ande Caleb <andaeiii@aol.com>
2020-12-16 14:37:19 +0100

    Semantic Ui-Setup..

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from "react-dom";
 import { BrowserRouter } from 'react-router-dom';
+import 'semantic-ui-css/semantic.min.css';      // add semantic ui-css here...
 import App from "./App";
 import reportWebVitals from './reportWebVitals';
 
+//enclose the app component in browser router
+//to make the router available within the application... 
+
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter>             
     <App/>
   </BrowserRouter>,
   document.getElementById("root")
